78bad847ebdf17e1d61cd4943da9a7e601459c21
Pádraig Ní Rúis <prushguitar@gmail.com>
2023-12-30 03:25:49 +0000

    Rework functionality and UI

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // VARIABLES
-// operation variables
-let firstNumber;
-let operator;
-let secondNumber;
+let previousNumber;
+let currentOperator;
+let currentNumber = "";
 
-// FUNCTIONS
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.getElementById("equals-btn");
+const decimalButton = document.getElementById("decimal-btn");
+const currentOperand = document.querySelector(".currentOperand");
+const previousOperand = document.querySelector(".previousOperand");
+
 // maths operator functions
 function add(num1, num2) {
     return num1 + num2;
@@ -22,11 +27,62 @@ function divide(num1, num2) {
     return num1 / num2;
 };
 
-// operate function
-function operate(num1, num2, operator) {
-    return operator(num1, num2);
+// button functions
+
+function operate(num1, operator, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    switch (operator) {
+        case "+":
+            return add(num1, num2)
+        case "-":
+            return subtract(num1, num2)
+        case "x":
+            return multiply(num1, num2)
+        case "/":
+            if (num2 === 0) {
+                return null
+            } else {
+                return divide(num1, num2)
+            }
+        default:
+            return null
+    }
 };
 
-// =====================================
-// UI STUFF
-// =====================================
+function setOperator(operator) {
+    if (currentOperator !== null) operate(previousNumber, currentNumber, currentOperator);
+
+    previousNumber = currentNumber;
+    currentOperator = operator;
+    currentNumber = "";
+};
+
+function setNumber(number) {
+    currentNumber += number;
+    currentOperand.textContent = currentNumber;
+}
+
+// other functions
+function evaluate() {
+    previousNumber = operate(previousNumber, currentOperator, currentNumber);
+    currentOperand.textContent = previousNumber;
+    
+    currentNumber = "";
+    currentOperator = "";
+}
+
+// =======================================
+// UI stuff
+// =======================================
+
+numberButtons.forEach(btn => {
+    btn.addEventListener("click", () => setNumber(btn.textContent))
+});
+
+operatorButtons.forEach(btn => {
+    btn.addEventListener("click", () => setOperator(btn.textContent))
+});
+
+equalsButton.addEventListener("click", () => evaluate());
